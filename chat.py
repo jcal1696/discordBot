0de667a7adba98a22396826993a05896308d831e
jcal1696 <jcal1696@gmail.com>
2018-10-18 09:54:32 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -4,10 +4,13 @@ import asyncio
 from bot         import discordBot
 from discord.ext import commands
 
+# set up discord client
 client = discord.Client()
 
+# create new discordBot object with desired parameters
 chatbot = discordBot(chattiness=1, chain_length=2, max_words=30, replies_to_generate=3)
 
+# when bot is ready, print to console username and id
 @client.event
 async def on_ready():
     print('Logged in as ')
@@ -15,9 +18,11 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+# when message is recieved, reply 
 @client.event
 async def on_message(message):
     channel = message.channel
+    # if the message came from the bot, don't reply
     if message.author == client.user:
         return
 
@@ -25,4 +30,5 @@ async def on_message(message):
         reply = chatbot.log(message.content)
         await client.send_message(channel,reply)
 
+# run the bot        
 client.run('insert discord bot token here')
